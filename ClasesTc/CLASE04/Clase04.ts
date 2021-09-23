70d18d04987bd1fb9b19b2478014452c85935300
DairibethRedondo <89915677+DairibethRedondo@users.noreply.github.com>
2021-09-22 22:10:33 -0500

    creacion de las listas

--- ClasesTc/CLASE04/Clase04.ts
+++ ClasesTc/CLASE04/Clase04.ts
@@ -3,3 +3,19 @@ let numero:number = 765;
 let nombre:string ="Diego";
 let apellido: string="pumarejo"
 
+
+console.log("numero");
+
+console.log (`el numero es${numero}`);
+console.log (`el nombre es${nombre}`);
+
+
+let grupo: string[]=["deilis rojas ", "dairibeth redondo","josue martinez", "diego pumarejo"];
+let lista: string[]=["Riohacha ", "Maicao","Dibulla"];
+console.log(lista)
+console.log(grupo);
+for (let index = 0; index < lista.length; index++) {
+    console.log(lista[index])
+    
+}
+
