cb72577187fa85ae448b7923b49c475b98451610
DairibethRedondo <89915677+DairibethRedondo@users.noreply.github.com>
2021-09-22 22:11:52 -0500

    Update Clase04.js

--- ClasesTc/CLASE04/Build/Clase04.js
+++ ClasesTc/CLASE04/Build/Clase04.js
@@ -3,3 +3,14 @@ console.log("hello word hp");
 let numero = 765;
 let nombre = "Diego";
 let apellido = "pumarejo";
+
+console.log("numero");
+console.log(`el numero es${numero}`);
+console.log(`el nombre es${nombre}`);
+let grupo = ["deilis rojas ", "dairibeth redondo", "josue martinez", "diego pumarejo"];
+let lista = ["Riohacha ", "Maicao", "Dibulla"];
+console.log(lista);
+console.log(grupo);
+for (let index = 0; index < lista.length; index++) {
+    console.log(lista[index]);
+}
