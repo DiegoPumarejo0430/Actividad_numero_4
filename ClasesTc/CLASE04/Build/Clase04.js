748a26e5f036f22c12c4e056e64785f9376a42a4
DIEGO ANDRES PUMAREJO PADILLA <89883027+DeliRojas2612@users.noreply.github.com>
2021-09-22 22:00:46 -0500

    se agrego typescript a la maquetacion

---
+++ ClasesTc/CLASE04/Build/Clase04.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log("hello word hp");
+let numero = 765;
+let nombre = "Diego";
+let apellido = "pumarejo";
---
+++ ClasesTc/CLASE04/Clase04.ts
@@ -0,0 +1,5 @@
+console.log("hello word hp");
+let numero:number = 765;
+let nombre:string ="Diego";
+let apellido: string="pumarejo"
+
