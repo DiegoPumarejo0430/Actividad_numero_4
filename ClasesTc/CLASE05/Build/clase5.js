e362ef25a1be6f79d8966c8432c4d3d93adc4fdc
DairibethRedondo <89915677+DairibethRedondo@users.noreply.github.com>
2021-10-05 22:08:38 -0500

    Update clase5.js

--- ClasesTc/CLASE05/Build/clase5.js
+++ ClasesTc/CLASE05/Build/clase5.js
@@ -30,3 +30,35 @@ function sumar2(x, y) {
         return suma;
     }
 }
+
+var suma01 = sumar2(2);
+var suma02 = sumar2(2, 3);
+console.log("el resultado de la suma 1 es =" + suma01);
+console.log("el resultado de la suma 2 es =" + suma02);
+function calcularEdad() {
+    return 18;
+}
+var mostrarPersona = function (nombre, Edad) {
+    if (Edad === void 0) { Edad = calcularEdad(); }
+    console.info("nombre : " + nombre + " - Edad : " + Edad);
+    if (Edad == 18) {
+        console.warn("la edad no fue enviada");
+    }
+};
+mostrarPersona("Diego");
+var duplicador = function (n) {
+    return n * 2;
+};
+var triplicador = function (n) {
+    return n * 3;
+};
+var potenciadorALaDos = function (n) {
+    return n * n;
+};
+function sumar50(f, y) {
+    return f(y) + 50;
+}
+console.log(sumar50(duplicador, 20));
+console.log(sumar50(triplicador, 20));
+console.log(sumar50(potenciadorALaDos, 20));
+console.log(sumar50(duplicador, 10));
